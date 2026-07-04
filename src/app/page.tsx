9aa3620411e4acf9c68aa6662a35cf88c87976d8
Ronaldofigr <ronaldofigr@hotmail.com>
2026-07-04 19:36:19 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
-import { getAllNews, getAllArticles } from '@/lib/content'
+import { getAllNews, getAllArticles, getPhotoWeek } from '@/lib/content'
 import HomeClient from '@/components/HomeClient'
 
 export default function Home() {
-  const news     = getAllNews()
-  const articles = getAllArticles()
-  return <HomeClient news={news} articles={articles} />
+  const news      = getAllNews()
+  const articles  = getAllArticles()
+  const photoWeek = getPhotoWeek()
+  return <HomeClient news={news} articles={articles} photoWeek={photoWeek} />
 }
