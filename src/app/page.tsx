ecb7e9b8076f96db20b4063258fdd5f48cb814dd
Ronaldofigr <ronaldofigr@hotmail.com>
2026-07-08 12:42:00 -0300

    Add moonInfo to Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import { getAllNews, getAllArticles, getPhotoWeek } from '@/lib/content'
+import { getAllNews, getAllArticles, getPhotoWeek, getMoonInfo } from '@/lib/content'
 import HomeClient from '@/components/HomeClient'
 
 export default function Home() {
   const news      = getAllNews()
   const articles  = getAllArticles()
   const photoWeek = getPhotoWeek()
-  return <HomeClient news={news} articles={articles} photoWeek={photoWeek} />
+  const moonInfo  = getMoonInfo()
+  return <HomeClient news={news} articles={articles} photoWeek={photoWeek} moonInfo={moonInfo} />
 }
