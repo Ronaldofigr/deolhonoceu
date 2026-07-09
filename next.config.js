4ecb8e8b3a49aab0270d55de7a31232b7eb5e1af
Ronaldofigr <ronaldofigr@hotmail.com>
2026-07-09 16:51:13 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,8 +3,6 @@ const nextConfig = {
   output: 'export',
   trailingSlash: true,
   images: { unoptimized: true },
-  basePath: '/deolhonoceu',
-  assetPrefix: '/deolhonoceu/',
 }
 
 module.exports = nextConfig
